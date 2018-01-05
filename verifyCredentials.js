83f557ba09d69c1ab10d4383e5e262ea6a8b0937
Renat Zubairov <renat.zubairov@gmail.com>
2018-01-05 12:12:55 +0100

    Added verifyCredentials

---
+++ verifyCredentials.js
@@ -0,0 +1,13 @@
+'use strict';
+const Podio = require('podio-js').api;
+// This function will be called by the platform to verify credentials
+module.exports =  function verifyCredentials(credentials) {
+    console.log('Credentials passed for verification %j', credentials);
+    const podio = new Podio({
+        authType: 'server',
+        clientId: process.env.PODIO_APP_ID,
+        clientSecret: process.env.PODIO_APP_SECRET
+    });
+    podio.setAccessToken(credentials.oauth);
+    return podio.isAuthenticated();
+};
